58f78393e8bac09b99726d8d68af8f6a6109d205
Nimisha Mavar <nimishamavar.0101@gmail.com>
2024-11-26 17:52:28 +0530

    requirements  added

--- GPT_app.py
+++ GPT_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from sentence_transformers import SentenceTransformer
 import openai
 from pinecone import Pinecone
-import google.generativeai as genai
 from datetime import datetime
 import pandas as pd
 import json
